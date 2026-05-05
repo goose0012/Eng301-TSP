546adfa69ff5452113acd9a39bef581b05b6760d
Kasen Davis <dkasen751@gmail.com>
2026-05-05 08:35:33 -0500

    All wifi stuff commented out
    Wifi isn't up, get out of loop!

--- Engr301-ELT9-starter-script.py
+++ Engr301-ELT9-starter-script.py
@@ -2,6 +2,7 @@
 #################### IMPORT LIBRARIES ######################
 ############################################################
 from machine import ADC
+from math import log
 from time import sleep                     # <<< DO NOT MODIFY >>>
 sleep(5) # required for stability          # <<< DO NOT MODIFY >>>
 
@@ -26,40 +27,40 @@ thermistor = ADC(28)
 ############################################################
 
 # Wi-Fi and MQTT settings
-SSID = "WilfongEngr301" # Raspberry Pi 4 Wi-Fi name                               # <<< DO NOT MODIFY >>>
-PASSWORD = "BoilerUp" # Raspberry Pi 4 Wi-Fi password, WPA/WPA2 security          # <<< DO NOT MODIFY >>>
-MQTT_BROKER = "10.42.0.1"  # Raspberry Pi 4's IP                                  # <<< DO NOT MODIFY >>>
-TOPIC = "pico/data" # "pico/data" is just a label                                 # <<< DO NOT MODIFY >>>
+# SSID = "WilfongEngr301" # Raspberry Pi 4 Wi-Fi name                               # <<< DO NOT MODIFY >>>
+# PASSWORD = "BoilerUp" # Raspberry Pi 4 Wi-Fi password, WPA/WPA2 security          # <<< DO NOT MODIFY >>>
+# MQTT_BROKER = "10.42.0.1"  # Raspberry Pi 4's IP                                  # <<< DO NOT MODIFY >>>
+# TOPIC = "pico/data" # "pico/data" is just a label                                 # <<< DO NOT MODIFY >>>
                     # It helps organize messages, like folders in a file system.  # <<< DO NOT MODIFY >>>
                     # The TOPIC could be any string, but leave it as "pico/data"  # <<< DO NOT MODIFY >>>
 
-SENSOR_ID = "Team02"  # !!!-- CHANGE THIS AS DIRECTED BY DR. WILFONG --!!!
+# SENSOR_ID = "Team02"  # !!!-- CHANGE THIS AS DIRECTED BY DR. WILFONG --!!!
 
 # Connect to Wi-Fi                                          # <<< DO NOT MODIFY >>>
-wlan = network.WLAN(network.STA_IF)                         # <<< DO NOT MODIFY >>>
-wlan.active(True)                                           # <<< DO NOT MODIFY >>>
-wlan.config(pm = 0xa11140) # disable Wi-Fi low power mode   # <<< DO NOT MODIFY >>>
-wlan.connect(SSID, PASSWORD)                                # <<< DO NOT MODIFY >>>
+# wlan = network.WLAN(network.STA_IF)                         # <<< DO NOT MODIFY >>>
+# wlan.active(True)                                           # <<< DO NOT MODIFY >>>
+# wlan.config(pm = 0xa11140) # disable Wi-Fi low power mode   # <<< DO NOT MODIFY >>>
+# wlan.connect(SSID, PASSWORD)                                # <<< DO NOT MODIFY >>>
 
-print("Attempting to connect to Wi-Fi")
-while not wlan.isconnected():                               # <<< DO NOT MODIFY >>>
-    pass                                                    # <<< DO NOT MODIFY >>>
+# print("Attempting to connect to Wi-Fi")
+# while not wlan.isconnected():                               # <<< DO NOT MODIFY >>>
+#    pass                                                    # <<< DO NOT MODIFY >>>
 
-sleep(2)  # Extra delay for stability                       # <<< DO NOT MODIFY >>>
-print("Connected to Wi-Fi!")
+# sleep(2)  # Extra delay for stability                       # <<< DO NOT MODIFY >>>
+# print("Connected to Wi-Fi!")
 
 
 
 # Connect to MQTT broker with reconnect support         # <<< DO NOT MODIFY >>>
-client = MQTTClient(f"client_{SENSOR_ID}", MQTT_BROKER) # <<< DO NOT MODIFY >>>
-client.DEBUG = True                                     # <<< DO NOT MODIFY >>>
+# client = MQTTClient(f"client_{SENSOR_ID}", MQTT_BROKER) # <<< DO NOT MODIFY >>>
+# client.DEBUG = True                                     # <<< DO NOT MODIFY >>>
 
 # Try to connect to MQTT broker                         # <<< DO NOT MODIFY >>>
-try:                                                    # <<< DO NOT MODIFY >>>
-    client.connect()                                    # <<< DO NOT MODIFY >>>
-    print("Connected to MQTT broker!")
-except Exception as e:                                  # <<< DO NOT MODIFY >>>
-    print("Failed to connect to MQTT broker:", e)
+# try:                                                    # <<< DO NOT MODIFY >>>
+#    client.connect()                                    # <<< DO NOT MODIFY >>>
+#    print("Connected to MQTT broker!")
+# except Exception as e:                                  # <<< DO NOT MODIFY >>>#
+# print("Failed to connect to MQTT broker:", e)
 
 # voltage divider
 V_in = 3.3 #[volts]
@@ -70,33 +71,32 @@ A = 1.129e-3
 B= 2.341e-4
 C= 8.767e-8
 
-def getTempC():
-        adc_value = thermistor.read_u16() # 0 to 65535
-        V_out = (V_in/65535) * adc_value #[volts]
-        Rt = (V_out * R1) / (V_in - V_out) # calculate resistance
-        TempK = 1 / (A + (B * log(Rt)) + (C*pow(log(Rt), 3) ) )
-        temperature_sensor_reading = TempK - 273.15 #[celcius]
-        return temperature_sensor_reading
-        print(temperature_sensor_reading)
 
 ############################################################
 ####################### INFINITE LOOP ######################
 ############################################################
+while True: 
+    #get temp
+    adc_value = thermistor.read_u16() # 0 to 65535
+    V_out = (V_in/65535) * adc_value #[volts]
+    Rt = (V_out * R1) / (V_in - V_out) # calculate resistance
+    TempK = 1 / (A + (B * log(Rt)) + (C*pow(log(Rt), 3) ) )
+    temperature_sensor_reading = TempK - 273.15 #[celcius]
+    print(temperature_sensor_reading)
 
-while True:
 
     # Create and send MQTT payload                               # <<< DO NOT MODIFY >>>
-    message_data = {                                             # <<< DO NOT MODIFY >>>
-        "sensorID": SENSOR_ID,                                   # <<< DO NOT MODIFY >>>
-        "temperatureReading": temperature_sensor_reading         # <<< DO NOT MODIFY >>>
-    }                                                            # <<< DO NOT MODIFY >>>
-    message_json = json.dumps(message_data)  # Convert to JSON   # <<< DO NOT MODIFY >>>
+#    message_data = {                                             # <<< DO NOT MODIFY >>>
+#        "sensorID": SENSOR_ID,                                   # <<< DO NOT MODIFY >>>
+#        "temperatureReading": temperature_sensor_reading         # <<< DO NOT MODIFY >>>
+#    }                                                            # <<< DO NOT MODIFY >>>
+#    message_json = json.dumps(message_data)  # Convert to JSON   # <<< DO NOT MODIFY >>>
     
     # Try to publish message to MQTT broker                                    # <<< DO NOT MODIFY >>>
-    try:                                                                       # <<< DO NOT MODIFY >>>
-        client.publish(TOPIC, message_json, retain=True) # Send MQTT payload   # <<< DO NOT MODIFY >>>
-        print(f"Published: {message_json}") # Print MQTT payload to the Shell
-    except Exception as e:                                                     # <<< DO NOT MODIFY >>>
-        print("Publish failed:",e)
+#    try:                                                                       # <<< DO NOT MODIFY >>>
+#        client.publish(TOPIC, message_json, retain=True) # Send MQTT payload   # <<< DO NOT MODIFY >>>
+#        print(f"Published: {message_json}") # Print MQTT payload to the Shell
+#    except Exception as e:                                                     # <<< DO NOT MODIFY >>>
+#        print("Publish failed:",e)
     
     sleep(2) # Send MQTT payload every 2 seconds
