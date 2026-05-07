5a96d38dbd99c74a092fe605c9980306ccd57bd1
Allen McDade <am100111001@gmail.com>
2026-05-07 13:15:06 -0500

    keypad copy paste
    ILL TAKE U DOWN TO MY WILFONGHOUSE

--- eng301allenkasennick.py
+++ eng301allenkasennick.py
@@ -12,6 +12,9 @@ import network                             # <<< DO NOT MODIFY >>>
 import json                                # <<< DO NOT MODIFY >>>
 from umqtt.robust import MQTTClient        # <<< DO NOT MODIFY >>>
 
+from machine import Pin
+import utime
+
 # Imports the library to make a random number. This is used to
 #    create a psuedo temperature value to transmit for demo
 #    purposes. You don't need this library for the project.
@@ -85,6 +88,43 @@ def getTempC():
     TempK = 1 / (A + (B * log(Rt)) + (C*pow(log(Rt), 3) ) )
     temperature_sensor_reading = TempK - 273.15 #[celcius]
     return temperature_sensor_reading
+# Create a map between keypad buttons and characters
+matrix_keys = [['1', '2', '3', 'A'],
+               ['4', '5', '6', 'B'],
+               ['7', '8', '9', 'C'],
+               ['*', '0', '#', 'D']]
+
+# PINs according to schematic - Change the pins to match with your connections
+keypad_rows = [9,8,7,6]
+keypad_columns = [5,4,3,2]
+
+# Create two empty lists to set up pins ( Rows output and columns input )
+col_pins = []
+row_pins = []
+
+# Loop to assign GPIO pins and setup input and outputs
+for x in range(0,4):
+    row_pins.append(Pin(keypad_rows[x], Pin.OUT))
+    row_pins[x].value(1)
+    col_pins.append(Pin(keypad_columns[x], Pin.IN, Pin.PULL_DOWN))
+    col_pins[x].value(0)
+    
+##############################Scan keys ####################
+    
+print("Please enter a key from the keypad")
+    
+def scankeys():  
+    for row in range(4):
+        for col in range(4):
+            row_pins[row].high()
+            key = None
+            
+            if col_pins[col].value() == 1:
+                print("You have pressed:", matrix_keys[row][col])
+                key_press = matrix_keys[row][col]
+                utime.sleep(0.3)
+                    
+        row_pins[row].low()
 
 ############################################################
 ####################### INFINITE LOOP ######################
@@ -117,4 +157,5 @@ while True:
 #        print("Publish failed:",e)
     
     sleep(2) # Send MQTT payload every 2 seconds
+
 
